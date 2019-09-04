778c58ffed6520b5559f8db6b1623eb1c4e1cee1
SeungYun Kim <melongbob@gmail.com>
2019-09-03 22:14:57 -0700

    Use uniform to make color change over time

--- GLProject/src/Application.cpp
+++ GLProject/src/Application.cpp
@@ -104,14 +104,16 @@ int main(void) {
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "OpenGL Tutorial", NULL, NULL);
 	if (!window) {
-		GLCall(glfwTerminate());
+		glfwTerminate();
 		return -1;
 	}
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
 
 	if (glewInit() != GLEW_OK) {
 		std::cout << "Error!" << std::endl;
@@ -148,22 +150,37 @@ int main(void) {
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	GLCall(glUseProgram(shader));
 
+	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+	ASSERT(location != -1);
+	GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+	float r = 0.0f;
+	float increment = 0.05f;
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
 		/* Render here */
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
+		if (r > 1.0f) {
+			increment = -0.05f;
+		}
+		else if (r < 0.0f)
+			increment = 0.05f;
+
+		r += increment;
+
 		/* Swap front and back buffers */
-		GLCall(glfwSwapBuffers(window));
+		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
-		GLCall(glfwPollEvents());
+		glfwPollEvents();
 	}
 
-	GLCall(glDeleteProgram(shader));
+	glDeleteProgram(shader);
 
-	GLCall(glfwTerminate());
+	glfwTerminate();
 	return 0;
 }
