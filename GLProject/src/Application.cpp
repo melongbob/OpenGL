96b5c37384c0bb549f4faf2c14829483bd8aaa9d
SeungYun Kim <melongbob@gmail.com>
2019-09-10 09:37:08 -0700

    move ClearColor test to the test framework

--- GLProject/src/Application.cpp
+++ GLProject/src/Application.cpp
@@ -21,7 +21,7 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
-#include "tests\TestClearColor.h"
+#include "tests/TestClearColor.h"
 
 int main(void) {
 	GLFWwindow* window;
@@ -63,21 +63,32 @@ int main(void) {
 		ImGui_ImplOpenGL3_Init();
 		ImGui::StyleColorsDark();
 
-		test::TestClearColor test;
+		test::Test* currentTest = nullptr;
+		test::TestMenu* testMenu = new test::TestMenu(currentTest);
+		currentTest = testMenu;
+
+		testMenu->RegisterTest<test::TestClearColor>("Clear Color");
 
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window)) {
 			/* Render here */
+			GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 			renderer.Clear();
-
-			test.OnUpdate(0.0f);
-			test.OnRender();
 
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
-
-			test.OnImGuiRender();
+			if (currentTest) {
+				currentTest->OnUpdate(0.0f);
+				currentTest->OnRender();
+				ImGui::Begin("Test");
+				if (currentTest != testMenu && ImGui::Button("<-")) {
+					delete currentTest;
+					currentTest = testMenu;
+				}
+				currentTest -> OnImGuiRender();
+				ImGui::End();
+			}
 
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -87,6 +98,10 @@ int main(void) {
 
 			/* Poll for and process events */
 			glfwPollEvents();
+		}
+		delete currentTest;
+		if (currentTest != testMenu) {
+			delete testMenu;
 		}
 	}
 	ImGui_ImplOpenGL3_Shutdown();
--- GLProject/src/tests/TestClearColor.cpp
+++ GLProject/src/tests/TestClearColor.cpp
@@ -3,7 +3,7 @@
 #include <GL/glew.h>
 
 #include "Renderer.h"
-#include "imgui\imgui.h"
+#include "imgui/imgui.h"
 
 namespace test {
 	TestClearColor::TestClearColor() 
